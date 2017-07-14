2413fc4888d991f980633e03186755de5b195db3
DESKTOP-CCDV18D\Administrator <757950169@qq.com>
2017-07-14 23:44:54 +0800

    no message

--- StarmileFx/src/StarmileFx.Wap.Server/Service/YoungoManager.cs
+++ StarmileFx/src/StarmileFx.Wap.Server/Service/YoungoManager.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StarmileFx.Models.Redis;
 using StarmileFx.Wap.Server.IService;
 
 namespace StarmileFx.Wap.Server.Service
 {
     public class YoungoManager : IYoungoServer
     {
+        public bool AddShopCart(int CustomerID, Dictionary<string, int> productList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddShopCartProduct(int CustomerID, string ProductID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteShopCart(int CustomerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShopCart GetShopCart(int CustomerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ModifyShopCart(int CustomerID, Dictionary<string, int> productList)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
