3bd1f4ae1d292710cc5b49bf42c66b32723d697c
cairongzhi <cairongzhi@banggood.cn>
2017-08-21 18:03:03 +0800

    no message

--- StarmileFx.Api/StarmileFx.Common/LogHelper.cs
+++ StarmileFx.Api/StarmileFx.Common/LogHelper.cs
@@ -2,6 +2,9 @@
 
 namespace StarmileFx.Common
 {
+    /// <summary>
+    /// 日志文件类
+    /// </summary>
     public static class LogHelper
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
@@ -9,19 +12,31 @@ namespace StarmileFx.Common
         {
         }
 
-        public static void Error(string message)
-        {
-            logger.Error(message);
-        }
-
-        public static void Warn(string message)
-        {
-            logger.Warn(message);
-        }
-
+        /// <summary>
+        /// 普通日志
+        /// </summary>
+        /// <param name="message"></param>
         public static void Info(string message)
         {
             logger.Info(message);
+        }
+
+        /// <summary>
+        /// 警告日志
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Warn(string message)
+        {
+            logger.Warn("<span class='orange'>" + message + "</span>");
+        }
+
+        /// <summary>
+        /// 错误日志
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Error(string message)
+        {
+            logger.Error("<span class='red'>" + message + "</span>");
         }
     }
 }
