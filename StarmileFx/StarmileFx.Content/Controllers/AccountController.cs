d1aa58a2d48993d699b482937d14df94ff63989f
cairongzhi <cairongzhi@banggood.cn>
2017-09-07 17:00:12 +0800

    2017年9月7日17:00:08

--- StarmileFx/StarmileFx.Content/Controllers/AccountController.cs
+++ StarmileFx/StarmileFx.Content/Controllers/AccountController.cs
@@ -51,6 +51,7 @@ namespace StarmileFx.Content.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
+        [Route("Login")]
         public async Task<IActionResult> Login([FromForm]LoginFrom fromData)
         {
             if (string.Compare(fromData.validCode, HttpContext.Session.GetString(SysConst.Captcha), true) != 0)
@@ -83,6 +84,7 @@ namespace StarmileFx.Content.Controllers
                         AllowRefresh = false
                     });
             }
+            string UserName = User.Claims.FirstOrDefault().Value;
             return Json(result);
         }
 
@@ -91,6 +93,7 @@ namespace StarmileFx.Content.Controllers
         /// </summary>
         /// <returns></returns>
         [AllowAnonymous]
+        [Route("Captcha")]
         public IActionResult Captcha()
         {
             System.IO.MemoryStream ms = VierificationCode.Create(out string code);
@@ -103,6 +106,7 @@ namespace StarmileFx.Content.Controllers
         /// 退出登录
         /// </summary>
         /// <returns></returns>
+        [Route("Logout")]
         public async Task<IActionResult> Logout()
         {
             string Token = User.Identities.First(u => u.IsAuthenticated).FindFirst("Token").Value;
