df154453692e58f62e9f66a27b77a8aa356e5419
cairongzhi <cairongzhi@banggood.cn>
2017-09-23 18:17:11 +0800

    2017年9月23日18:17:06

--- StarmileFx.vuejs/src/router/index.js
+++ StarmileFx.vuejs/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import login from '@/components/login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'Hello',
+            component: Hello
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: login
+        }
+    ]
 })
